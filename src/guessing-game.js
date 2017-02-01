84618fd68def96b882b616241b7c18775cddb444
ValentinChernyshov <valentino93@list.ru>
2017-02-01 17:53:28 +0200

    guessing-game/solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+      this.min = min
+      this.max = max
+      this.guessed
+    }
 
     setRange(min, max) {
-
+      this.min = min
+      this.max = max
     }
 
     guess() {
-
+      this.guessed = Math.round((
+        this.min + (this.max - this.min) / 2
+      ))
+      return this.guessed;
     }
 
     lower() {
-
+      this.setRange(this.min, this.guessed);
     }
 
     greater() {
-
+      this.setRange(this.guessed, this.max);
     }
 }
 
